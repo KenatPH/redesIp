3a06647f45bc3afcb4ceb1b70063d72e10537c7e
krtabares <krtabares@gmail.com>
2024-09-16 12:01:20 -0400

    add env variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,24 +4,35 @@ import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { RoleModule } from './modules/role/role.module';
 import { MailService } from './mail/mail.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mongodb',
-      url: 'mongodb://localhost:27017/ecommerceRedesIp',
-      synchronize: true,
-      useUnifiedTopology: true,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    ConfigModule.forRoot({
+      isGlobal: true, // Hace que las variables estén disponibles globalmente
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'mongodb',
+    //   url: ConfigService.get<string>('MONGODB_URL'),
+    //   synchronize: true,
+    //   useUnifiedTopology: true,
+    //   entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    // }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mongodb',
+        url: configService.get<string>('MONGODB_URI'),
+        synchronize: true,
+        useUnifiedTopology: true,
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      }),
     }),
     UserModule,
     RoleModule,
     AuthModule,
-    ConfigModule.forRoot({
-      isGlobal: true, // Hace que las variables estén disponibles globalmente
-    }),
   ],
   providers: [MailService],
 })
