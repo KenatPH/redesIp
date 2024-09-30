8936e7dd86e68016ff3db86c1cc78e68fabe0c46
krtabares <krtabares@gmail.com>
2024-09-30 12:42:49 -0400

    address

--- src/modules/address/address.module.ts
+++ src/modules/address/address.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Address } from 'src/entities/address.entity';
 import { User } from 'src/entities/user.entity';
 import { AddressService } from './address.service';
 import { AddressController } from './address.controller';
+import { Address } from 'src/entities/address.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Address, User])],
